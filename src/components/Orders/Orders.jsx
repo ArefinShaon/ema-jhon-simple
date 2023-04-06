7fb70c42ddeeaaa1625bd8f63feacfa6e8621bbd
ArefinShaon <arefinshaon99@gmail.com>
2023-04-06 23:54:56 +0600

    implement delete button

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Cart from '../Cart/Cart';
+import './Order.css'
+import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import  { removeFromDb } from '../../utilities/fakedb'
 const Orders = () => {
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart)
+    const handleRemoveCart = (id) => {
+        console.log(id);
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id)
+    }
+    
     return (
-        <div>
-            <h3>Orders</h3>
+        <div className='shop-container'>
+            <div className='review-container'>
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                        handleRemoveCart={handleRemoveCart}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
